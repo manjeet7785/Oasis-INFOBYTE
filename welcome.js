2a68e4d8887b75d6f38d44d0924266c8f256dec0
manjeet7785 <manjeetmaurya7785@gmail.com>
2024-09-17 22:07:29 +0530

    Login page

---
+++ welcome.js
@@ -0,0 +1,14 @@
+const username = localStorage.getItem('loggedInUser');
+const welcomeMessage = document.getElementById('welcome-message');
+welcomeMessage.textContent = `Welcome, ${
+}!`;
+const logoutBtn = document.getElementById('logout-btn');
+
+logoutBtn.addEventListener('click', () => {
+    localStorage.removeItem('loggedInUser');
+    window.location.href = 'index.html';
+});
+
+if (!username) {
+    window.location.href = 'index.html';
+}
